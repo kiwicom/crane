cb17c6591fa528bf18824ea205f7a658e55dfc27
Simone Esposito <chaufnet@gmail.com>
2018-08-07 14:37:27 +0200

    Add limited mode
    Addresses running in a non-Git repo directory and tagging Docker images
    with non-Git references.
    
    Closes https://github.com/kiwicom/crane/issues/7
    Closes https://github.com/kiwicom/crane/issues/8

--- crane/hooks/__init__.py
+++ crane/hooks/__init__.py
@@ -1,4 +1,5 @@
 from . import datadog, echo, sentry, slack, webhook
+from .. import deployment
 
 AVAILABLE_HOOKS = [datadog, echo, sentry, slack, webhook]
 
@@ -6,6 +7,9 @@ HOOKS = []
 
 
 def dispatch(event):
+    if deployment.is_limited:
+        return
+
     if not HOOKS:
         HOOKS.extend(getattr(module, 'Hook')() for module in AVAILABLE_HOOKS)
 
