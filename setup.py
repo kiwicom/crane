5d34c0d590b3a5aee16ec197c9f025da85d9829e
Bence Nagy <bence@underyx.me>
2018-08-20 17:27:00 +0200

    setup.py: Add Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: System :: Software Distribution",
     ],
 )
