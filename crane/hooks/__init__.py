05ffe9915ed85620054a93ea3ecc33879135a087
Bence Nagy <bence@underyx.me>
2019-04-03 12:20:47 +0200

    hooks: Handle errors upon init of hooks
    Closes https://github.com/kiwicom/crane/issues/26

--- crane/hooks/__init__.py
+++ crane/hooks/__init__.py
@@ -1,3 +1,7 @@
+import traceback
+
+import click
+
 from . import datadog, echo, sentry, slack, webhook
 from .. import deployment
 
@@ -11,7 +15,22 @@ def dispatch(event):
         return
 
     if not HOOKS:
-        HOOKS.extend(getattr(module, "Hook")() for module in AVAILABLE_HOOKS)
+        for module in AVAILABLE_HOOKS:
+            hook_class = getattr(module, "Hook")
+            try:
+                HOOKS.append(hook_class())
+            except:
+                click.secho(
+                    f"Oh, this is bad. I cannot load the '{module}' hook! "
+                    + "Even if you weren't planning to use it, this should never happen. "
+                    + "Please tell the maintainers about this at https://github.com/kiwicom/crane/issues/new "
+                    + "— sorry about all the trouble!"
+                    + click.style("(シ_ _)シ", bold=True),
+                    fg="red",
+                    err=True,
+                )
+                click.secho("Here's the traceback:\n", fg="red", err=True)
+                traceback.print_exc()
 
     for hook in HOOKS:
         hook.dispatch(event)
